3aa74ec4d1df1fed3196cd1bfc33379f8f6c1cde
Michele-Zhu <71699139+Michele-Zhu@users.noreply.github.com>
2025-10-12 21:25:05 +0000

    Deploying to gh-pages from @ Michele-Zhu/Michele-Zhu.github.io@d1a9923085ea2162ccc238cd41f01d41721bf8c1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -102,35 +102,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/michele-zhu/feed.xml", "_blank");
+          window.open("mailto:%6D%69%63%68%65%6C%65.%7A%68%75@%70%6F%6C%69%6D%69.%69%74", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=GEI6DqMAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://orcid.org/0009-0003-7448-559X", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/Michele-Zhu", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/michele-zhu-a17818224", "_blank");
         },
       },{
       id: 'light-theme',

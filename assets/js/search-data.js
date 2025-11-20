24cc048dec5d098f69d076b39480a9f112c9b785
Michele-Zhu <71699139+Michele-Zhu@users.noreply.github.com>
2025-11-20 16:57:33 +0000

    Deploying to gh-pages from @ Michele-Zhu/michele-zhu.github.io@601c8bc748078ba9dc0d42ea9c9982329e8ee6b9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This Page is under construction. N.B. you can opt out from having this page, just send the cv.",
+          description: "This page contains a brief of my vitae, for the extended version please send me and e-mail.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -46,6 +46,18 @@ ninja.data = [{
               },
             },{id: "news-i-started-my-phd-journey",
           title: 'I started my PhD journey!',
+          description: "",
+          section: "News",},{id: "news-the-first-weeks-of-july-have-been-crazy-the-first-week-i-attended-the-cost-5th-training-school-and-the-second-week-i-presented-my-first-paper-at-meditcom2024-everything-while-i-was-in-a-wheelchair-i-broke-my-foot-at-the-end-of-june-p-s-it-took-months-but-i-am-completely-fine",
+          title: 'The first weeks of July have been crazy. The first week, I attended...',
+          description: "",
+          section: "News",},{id: "news-toward-real-time-digital-twins-of-em-environments-computational-benchmark-for-ray-launching-software-in-ieee-open-journal-of-the-communication-society-is-online",
+          title: 'Toward Real-Time Digital Twins of EM Environments: Computational Benchmark for Ray Launching Software...',
+          description: "",
+          section: "News",},{id: "news-semantic-communication-via-features-identificaiton-in-ieee-international-conference-on-communication-is-online",
+          title: 'Semantic Communication via Features Identificaiton in IEEE International Conference on Communication is online!...',
+          description: "",
+          section: "News",},{id: "news-exploiting-age-of-information-in-network-digital-twins-for-ai-driven-real-time-link-blockage-detection-in-elservier-computer-networks-is-online",
+          title: 'Exploiting age of information in network digital twins for AI-driven real-time link blockage...',
           description: "",
           section: "News",},{
         id: 'social-email',
